c185215c17205d0c3a36d1930a7270130407a963
Maes95 <michel.maes@urjc.es>
2022-09-27 08:15:35 +0000

    Work on Compress bugs

---
+++ configFiles/Compress/build_fixes/fix-build-file.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+cp /home/regseek/workdir/configFiles/Compress/build_fixes/default.build.xml build.xml
+cp /home/regseek/workdir/configFiles/Compress/build_fixes/maven-build.xml maven-build.xml
