32c48d05be67d8b8db464f570e16905f42653d67
Maes95 <michel.maes@urjc.es>
2023-08-01 00:52:27 +0000

    Fix some issues on bugs from proects Math, JxPath and Compress

---
+++ configFiles/JxPath/build_fixes/Bug_16/fix-bug_16.sh
@@ -0,0 +1,3 @@
+cp /home/regseek/workdir/configFiles/JxPath/build_fixes/default.build.xml build.xml
+# Copy abstract class XMLModelTestCase, that really includes the regression test
+cp /home/regseek/workdir/configFiles/JxPath/build_fixes/Bug_16/XMLModelTestCase.java src/test/org/apache/commons/jxpath/ri/model/XMLModelTestCase.java
---
+++ configFiles/JxPath/build_fixes/Bug_18/fix-bug_18.sh
@@ -0,0 +1,4 @@
+cp /home/regseek/workdir/configFiles/JxPath/build_fixes/default.build.xml build.xml
+# Copy abstract class XMLModelTestCase, that really includes the regression test
+cp /home/regseek/workdir/configFiles/JxPath/build_fixes/Bug_18/XMLModelTestCase.java src/test/org/apache/commons/jxpath/ri/model/XMLModelTestCase.java
+cp /home/regseek/workdir/configFiles/JxPath/build_fixes/Bug_18/JXPathTestCase.java src/test/org/apache/commons/jxpath/JXPathTestCase.java
